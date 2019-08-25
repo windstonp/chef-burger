0d34c09f3fa22659f983cc46e3e874fc39e74af9
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2019-08-25 14:44:50 -0300

    Responsividade da home concluída

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	if ($(window).width() <= "992px") {
+		// $('.content2-infos').addClass('col-lg-12').removeClass('col-lg-6');
+	}
+
 	$('.burger').click(function() {
 		$('.menu-mobile-items').slideToggle('fast', this.checked);
 	});
